92664b28694dd826d48ab0f7b8765202e5037f70
Timothy Dowling <timothy.dowling@me.com>
2018-02-10 22:52:20 -0500

    Got a working gameboard to randomally display an 8x8 grid of gems

---
+++ Project-Match3/Assets/Scripts/GameController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject GameBoardPreFab;
+	public List<GameObject> GemPreFabs;
+
+	const int NUM_COLS = 8;
+	const int NUM_ROWS = 8;
+
+	GameObject board;
+
+	// Use this for initialization
+	void Start () {
+		board = Instantiate (GameBoardPreFab, this.transform);
+		board.name = "GAMEBOARD";
+
+		InitializeBoard ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void InitializeBoard(){
+
+		for (int col = 0; col < NUM_COLS; col++) {
+			Transform columnTransform = board.transform.GetChild(col);
+			for (int row = 0; row < NUM_ROWS; row++) {
+				Transform rowTransform = columnTransform.GetChild(row);
+				int randomSprite = Random.Range (0, GemPreFabs.Count);
+				Instantiate (GemPreFabs [randomSprite],
+					rowTransform.position,
+					Quaternion.identity,
+					rowTransform);
+			}
+		}
+	}
+}
